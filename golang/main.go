bef32651dfddbfb2b27dea3091a13605e21c1a32
Conan <conan@ahnlab.io>
2024-10-17 15:53:03 +0900

    fix

--- golang/main.go
+++ golang/main.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 
 	"github.com/ahnlabio/waas-example.git/golang/login"
+	"github.com/ahnlabio/waas-example.git/golang/mpc"
+	securechannel "github.com/ahnlabio/waas-example.git/golang/secureChannel"
+	"github.com/ahnlabio/waas-example.git/golang/signup"
 )
 
 func main() {
 	fmt.Println("==========start waas docs scenario==========")
-	// securechannel.SecureChannelScenario()
-	// signup.SignupScenario()
+	securechannel.SecureChannelScenario()
+	signup.SignupScenario()
 	login.LoginScenario()
-	// mpc.MPCScenario()
+	mpc.MPCScenario()
 	fmt.Println("==========end waas docs scenario==========")
 }
